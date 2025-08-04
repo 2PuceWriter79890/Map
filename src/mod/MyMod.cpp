89bd6b2ae4069de2d7880242e49d74b09b8e4c0a
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-08-04 19:12:13 +0800

    更新 MyMod.cpp

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,32 +1,51 @@
 #include "mod/MyMod.h"
 
-#include "ll/api/mod/RegisterHelper.h"
+#include "ll/api/command/CommandRegistrar.h"
+#include "ll/api/io/Logger.h"
+#include "ll/api/service/Bedrock.h"
+#include "mc/world/item/MapItem.h"
+#include "mc/world/level/saveddata/maps/MapItemSavedData.h"
 
-namespace my_mod {
+void registerMapInfoCommand() {
+    auto& cmd = ll::command::CommandRegistrar::getInstance().getOrCreateCommand(
+        "getmapinfo",
+        "获取手持地图信息",
+        CommandPermissionLevel::Any
+    );
 
-MyMod& MyMod::getInstance() {
-    static MyMod instance;
-    return instance;
+    cmd.overload().execute<[&](CommandOrigin const& origin, CommandOutput& output) {
+        if (auto* player = origin.getPlayer()) {
+            ItemStack* heldItem = player->getHandSlot();
+            
+            if (!heldItem || !heldItem->isMap()) {
+                output.error("请手持一张地图！");
+                return 0;
+            }
+
+            auto mapId = MapItem::getMapId(*heldItem);
+            
+            auto* mapData = ll::service::getLevel()->getMapSavedData().fetchSavedData(mapId);
+            
+            if (!mapData) {
+                output.error("无法获取地图数据！");
+                return 0;
+            }
+
+            output.success("=== 地图信息 ===");
+            output.success(fmt::format("地图ID: {}", mapId));
+            output.success(fmt::format("地图等级(比例尺): {}", mapData->getScale()));
+            output.success(fmt::format("是否上锁: {}", heldItem->isLocked() ? "是" : "否"));
+            output.success(fmt::format("地图中心坐标: X={}, Z={}", mapData->getCenterX(), mapData->getCenterZ()));
+            
+        } else {
+            output.error("只有玩家可以执行此命令");
+        }
+        return 0;
+    }>();
 }
 
-bool MyMod::load() {
-    getSelf().getLogger().debug("Loading...");
-    // Code for loading the mod goes here.
-    return true;
+void plugin_init() {
+    static ll::io::Logger logger("MapInfo");
+    registerMapInfoCommand();
+    logger.info("地图信息命令已注册 - /getmapinfo");
 }
-
-bool MyMod::enable() {
-    getSelf().getLogger().debug("Enabling...");
-    // Code for enabling the mod goes here.
-    return true;
-}
-
-bool MyMod::disable() {
-    getSelf().getLogger().debug("Disabling...");
-    // Code for disabling the mod goes here.
-    return true;
-}
-
-} // namespace my_mod
-
-LL_REGISTER_MOD(my_mod::MyMod, my_mod::MyMod::getInstance());
