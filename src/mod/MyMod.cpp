75dba50dbbc02b5e51549fb44fb7043a03cafa57
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-08-09 22:16:21 +0800

    Update MyMod.cpp

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,71 +1,72 @@
 #include "mod/MyMod.h"
 
-#include <ll/api/mod/NativeMod.h>
-#include <ll/api/event/EventBus.h>
-#include <ll/api/event/Listener.h>
-#include <ll/api/event/player/PlayerDieEvent.h>
-#include <ll/api/event/player/PlayerRespawnEvent.h>
+#include "MyMod.h"
+#include "config.h"
 
-#include <mc/world/actor/player/Player.h>
-#include <mc/deps/core/math/Vec3.h>
-#include <mc/nbt/CompoundTag.h>
-#include <mc/nbt/FloatTag.h>
-#include <mc/nbt/IntTag.h>
+#include "ll/api/event/EventBus.h"
+#include "ll/api/event/player/PlayerDieEvent.h"
+#include "ll/api/event/player/PlayerRespawnEvent.h"
+#include "mc/world/actor/player/Player.h"
 
-#include <map>
-#include <string>
-#include <set>
-#include <memory>
+namespace my_mod {
 
-static const std::set<std::string> allowedPlayers = {
-    "Steve",
-    "Alex"
-};
+MyMod& MyMod::getInstance() {
+    static MyMod instance;
+    return instance;
+}
 
-static std::map<std::string, std::unique_ptr<CompoundTag>> deathInfo;
+bool MyMod::load() {
+    loadAndCreateDefaultConfig(getSelf());
+    return true;
+}
 
-void plugin_entry(ll::mod::NativeMod& self) {
-    auto playerDieListener = ll::event::Listener<ll::event::PlayerDieEvent>::create(
-        [&](ll::event::PlayerDieEvent& event) {
+bool MyMod::enable() {
+    auto& eventBus = ll::event::EventBus::getInstance();
+
+    mPlayerDieListener = ll::event::Listener<ll::event::PlayerDieEvent>::create(
+        [this](ll::event::PlayerDieEvent& event) {
             auto& player = event.self();
             
-            if (allowedPlayers.count(player.getRealName())) {
-                auto uuidStr = player.getUuid().asString();
-                auto position = player.getPosition();
-                auto dimensionId = player.getDimensionId();
-
-                auto tag = std::make_unique<CompoundTag>();
-                
-                tag->mTags.emplace("x", FloatTag::create(position.x));
-                tag->mTags.emplace("y", FloatTag::create(position.y));
-                tag->mTags.emplace("z", FloatTag::create(position.z));
-                tag->mTags.emplace("dim", IntTag::create(dimensionId.id));
-
-                deathInfo[uuidStr] = std::move(tag);
+            if (gAllowedPlayers.count(player.getRealName())) {
+                int totalXp = player.getTotalExperience();
+                mStoredExperience[player.getUuid().asString()] = totalXp;
+                player.addExperience(-totalXp);
+                player.addLevels(-player.getPlayerLevel());
+                player.setExperience(0);
             }
             return true;
         }
     );
-    ll::event::EventBus::getInstance().addListener(playerDieListener);
 
-    auto playerRespawnListener = ll::event::Listener<ll::event::PlayerRespawnEvent>::create(
-        [&](ll::event::PlayerRespawnEvent& event) {
+    mPlayerRespawnListener = ll::event::Listener<ll::event::PlayerRespawnEvent>::create(
+        [this](ll::event::PlayerRespawnEvent& event) {
             auto& player = event.self();
             auto uuidStr = player.getUuid().asString();
 
-            auto it = deathInfo.find(uuidStr);
-            if (it != deathInfo.end()) {
-                auto const& tag = it->second;
-
-                Vec3 savedPos(tag->getFloat("x"), tag->getFloat("y"), tag->getFloat("z"));
-                DimensionId savedDim(tag->getInt("dim"));
-                
-                player.teleportTo(Vec3(savedPos.x, savedPos.y + 0.5, savedPos.z), true, 0, 0, false);
-
-                deathInfo.erase(it);
+            auto it = mStoredExperience.find(uuidStr);
+            if (it != mStoredExperience.end()) {
+                player.addExperience(it->second);
+                mStoredExperience.erase(it);
             }
             return true;
         }
     );
-    ll::event::EventBus::getInstance().addListener(playerRespawnListener);
+
+    eventBus.addListener(mPlayerDieListener);
+    eventBus.addListener(mPlayerRespawnListener);
+    
+    return true;
+}
+
+bool MyMod::disable() {
+    auto& eventBus = ll::event::EventBus::getInstance();
+
+    eventBus.removeListener(mPlayerDieListener);
+    eventBus.removeListener(mPlayerRespawnListener);
+
+    mStoredExperience.clear();
+
+    return true;
+}
+
 }
