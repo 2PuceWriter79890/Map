868518de560cf2e55311f87bbd5afa5f53718a88
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-08-09 18:25:12 +0800

    Update MyMod.cpp

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,117 +1,64 @@
 #include "mod/MyMod.h"
 
-#include "ll/api/command/CommandRegistrar.h"
-#include "mc/nbt/CompoundTag.h"
-#include "gmlib/mc/world/Level.h"
-#include "gmlib/mc/world/actor/OfflinePlayer.h"
-#include "gmlib/mc/world/actor/Player.h"
+#include <ll/api/plugin/NativePlugin.h>
+#include <ll/api/event/EventBus.h>
+#include <ll/api/event/player/PlayerDieEvent.h>
+#include <ll/api/event/player/PlayerRespawnEvent.h>
 
-namespace {
+#include <mc/world/actor/player/Player.h>
+#include <mc/deps/core/math/Vec3.h>
+#include <mc/nbt/CompoundTag.h>
 
-// 清理单个玩家的物品
-void cleanPlayerItems(gmlib::GMPlayer& player, const std::string& itemId) {
-    int removed = 0;
-    
-    // 获取玩家所有物品
-    auto items = player.getItems(itemId);
-    
-    // 清理物品
-    for (auto& itemRef : items) {
-        if (itemRef && itemRef->getTypeName() == itemId) {
-            removed += itemRef->getCount();
-            itemRef->setCount(0);
-        }
-    }
-    
-    if (removed > 0) {
-        player.sendText(fmt::format("Removed {} {} from your inventory", removed, itemId));
-    }
-}
+#include <map>
+#include <string>
+#include <set>
+#include <memory>
 
-// 清理所有玩家(在线和离线)
-void cleanAllPlayersItems(const std::string& itemId) {
-    int totalRemoved = 0;
-    
-    // 1. 处理在线玩家
-    auto onlinePlayers = gmlib::GMLevel::getInstance()->getAllPlayers();
-    for (auto player : onlinePlayers) {
-        cleanPlayerItems(*player, itemId);
-    }
-    
-    // 2. 处理离线玩家
-    auto level = gmlib::GMLevel::getInstance();
-    if (!level) return;
-    
-    // 获取所有玩家数据文件
-    auto playerDataPath = "./world/playerdata/";
-    auto files = ll::file::getAllFiles(playerDataPath, false);
-    
-    for (const auto& file : files) {
-        if (file.ends_with(".dat")) {
-            try {
-                // 读取玩家数据
-                auto uuid = file.substr(0, file.size() - 4);
-                auto playerData = gmlib::GMPlayer::getOfflinePlayerData(uuid);
-                
-                if (!playerData) continue;
-                
-                // 处理背包物品
-                if (playerData->contains("Inventory")) {
-                    auto& inventory = playerData->getList("Inventory");
-                    for (int i = inventory.size() - 1; i >= 0; --i) {
-                        auto item = inventory.getCompound(i);
-                        if (item.getString("id") == itemId) {
-                            totalRemoved += item.getByte("Count", 1);
-                            inventory.remove(i);
-                        }
-                    }
-                }
-                
-                // 处理末影箱物品
-                if (playerData->contains("EnderItems")) {
-                    auto& enderItems = playerData->getList("EnderItems");
-                    for (int i = enderItems.size() - 1; i >= 0; --i) {
-                        auto item = enderItems.getCompound(i);
-                        if (item.getString("id") == itemId) {
-                            totalRemoved += item.getByte("Count", 1);
-                            enderItems.remove(i);
-                        }
-                    }
-                }
-                
-                // 保存修改后的数据
-                gmlib::GMPlayer::saveOfflinePlayerData(uuid, *playerData);
-                
-            } catch (...) {
+static const std::set<std::string> allowedPlayers = {
+    "Steve",
+    "Alex"
+};
+
+static std::map<std::string, std::unique_ptr<CompoundTag>> deathInfo;
+
+void plugin_entry(ll::plugin::NativePlugin& self) {
+    ll::event::EventBus::getInstance().subscribe<ll::event::PlayerDieEvent>(
+        [&](ll::event::PlayerDieEvent& event) {
+            auto& player = event.self();
+            
+            if (allowedPlayers.count(player.getRealName())) {
+                auto uuidStr = player.getUuid().asString();
+                auto position = player.getPosition();
+                auto dimensionId = player.getDimensionId();
+
+                auto tag = std::make_unique<CompoundTag>();
+                tag->putFloat("x", position.x);
+                tag->putFloat("y", position.y);
+                tag->putFloat("z", position.z);
+                tag->putInt("dim", dimensionId.id);
+
+                deathInfo[uuidStr] = std::move(tag);
             }
+            return true;
         }
-    }
-    
-    // 广播清理结果
-    if (totalRemoved > 0) {
-        auto msg = fmt::format("Cleaned total {} {} from all players", totalRemoved, itemId);
-        level->broadcast(msg);
-    }
-}
-
-// 注册命令
-void registerCommand() {
-    auto& cmd = ll::command::CommandRegistrar::getInstance().getOrCreateCommand(
-        "cleanitems",
-        "Remove specified item from all players' inventories",
-        CommandPermissionLevel::GameDirectors
     );
-    
-    cmd.overload<std::string>()
-        .required("itemId")
-        .execute([](CommandOrigin const& origin, CommandOutput& output, std::string const& itemId) {
-            cleanAllPlayersItems(itemId);
-            output.success(fmt::format("Started cleaning {} from all players", itemId));
-        });
-}
 
-} // namespace
+    ll::event::EventBus::getInstance().subscribe<ll::event::PlayerRespawnEvent>(
+        [&](ll::event::PlayerRespawnEvent& event) {
+            auto& player = event.self();
+            auto uuidStr = player.getUuid().asString();
 
-void PluginInit() {
-    registerCommand();
+            auto it = deathInfo.find(uuidStr);
+            if (it != deathInfo.end()) {
+                auto const& tag = it->second;
+                Vec3 savedPos(tag->getFloat("x"), tag->getFloat("y"), tag->getFloat("z"));
+                DimensionId savedDim(tag->getInt("dim"));
+                
+                player.teleport(Vec3(savedPos.x, savedPos.y + 0.5, savedPos.z), savedDim);
+
+                deathInfo.erase(it);
+            }
+            return true;
+        }
+    );
 }
